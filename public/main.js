47ffeeaa98c9a4f9bae87a0b847a69d00c3710b7
Simon Dein <simon.dein@gmail.com>
2020-12-02 00:53:28 +0100

    ace config moved to main js file for easier mantainance

--- public/main.js
+++ public/main.js
@@ -1,17 +1,22 @@
 import {mouseRecorder} from './modules/mouseRecorder.js';
-import MultiRecorder from './modules/multiRecorder.js'
-import AudioRecorder from './modules/audioRecorder.js'
 
-const recStartButton = document.querySelector('#start');
+const startButton = document.querySelector('#start');
 const playButton = document.querySelector('#play');
-const recStopButton = document.querySelector('#stop');
+const stopButton = document.querySelector('#stop');
 const durationDisplay = document.querySelector('#duration');
 const audioPlayer = document.querySelector('#audio-player');
 
+<<<<<<< HEAD
+// ace is imported into the global namespace from 'ace-builds/src-noconflict/ace.js' in index.html
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/gruvbox");
+editor.session.setMode("ace/mode/javascript");
 
 const audioRecorder = new AudioRecorder;
 const multirecorder = new MultiRecorder(recStartButton, recStopButton, audioRecorder, editorRecorder);
 
+=======
+>>>>>>> parent of a5120b8... Extracted audio recording to own module. All recording is now done through the multi recorder
 let secondsPassed = 0
 let interval
 const mouseRecorder1 = Object.create(mouseRecorder).init();
@@ -38,51 +43,51 @@ let audioChunks = [];
 // })();
 
 // START Button
-// recStartButton.onclick = () => {
-//   interval = window.setInterval(increaseDuration, 1000, durationDisplay) // Increment time each second and display it
-//   mouseRecorder1.record();
-//   // mediaRecorder.start();
-// }
+startButton.onclick = () => {
+  interval = window.setInterval(increaseDuration, 1000, durationDisplay) // Increment time each second and display it
+  mouseRecorder1.record();
+  // mediaRecorder.start();
+}
 
-// // STOP button
-// recStopButton.onclick = () => {
-//   clearInterval(interval);
-//   durationDisplay.innerHTML = '00:00';
+// STOP button
+stopButton.onclick = () => {
+  clearInterval(interval);
+  durationDisplay.innerHTML = '00:00';
 
-//   mouseRecorder1.stop();
-//   // mediaRecorder.stop();
-// }
+  mouseRecorder1.stop();
+  // mediaRecorder.stop();
+}
 
-// // PLAY button
-// playButton.onclick = () => {
-//   mouseRecorder1.play();
-// }
+// PLAY button
+playButton.onclick = () => {
+  mouseRecorder1.play();
+}
 
-// function increaseDuration() {
-//   secondsPassed += 1;
+function increaseDuration() {
+  secondsPassed += 1;
 
-//   let display = document.querySelector('#duration');
-//   display.innerHTML = format(secondsPassed);
-// }
+  let display = document.querySelector('#duration');
+  display.innerHTML = format(secondsPassed);
+}
 
-// // ==========================
-// // Helper functions
-// function format(seconds) {
-//   let minutes = String(Math.floor(seconds / 60));
-//   seconds = String(seconds % 60);
+// ==========================
+// Helper functions
+function format(seconds) {
+  let minutes = String(Math.floor(seconds / 60));
+  seconds = String(seconds % 60);
 
-//   let minutesPadding = minutes.length > 1 ? '' : '0';
-//   let secondsPadding = seconds.length > 1 ? '' : '0';
+  let minutesPadding = minutes.length > 1 ? '' : '0';
+  let secondsPadding = seconds.length > 1 ? '' : '0';
 
-//   return `${minutesPadding}${minutes}:${secondsPadding}${seconds}`;
-// }
+  return `${minutesPadding}${minutes}:${secondsPadding}${seconds}`;
+}
 
-// async function getAudioStream() {
-//   try {
-//     let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-//     console.log(stream);
-//     return stream;
-//   } catch(error) {
-//     console.log('An audio stream could not be created', error);
-//   }
-// }
+async function getAudioStream() {
+  try {
+    let stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    console.log(stream);
+    return stream;
+  } catch(error) {
+    console.log('An audio stream could not be created', error);
+  }
+}
