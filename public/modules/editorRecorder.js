ebacc03ca68b622adc5671d088a8041f07721425
Simon Dein <simon.dein@gmail.com>
2020-12-03 21:07:53 +0100

    Cleaning up for new codemirror api

--- public/modules/editorRecorder.js
+++ public/modules/editorRecorder.js
@@ -1,27 +1,11 @@
 function EditorRecorder (editor) {
   this.editor = editor;
-  this.editor.setTheme("ace/theme/gruvbox");
-  this.editor.session.setMode("ace/mode/javascript");
   this.events = [];
   console.log(this.editor)
   this.initialize();
 };
 
 EditorRecorder.prototype.initialize = function () {
-  this.editor.session.on('change', function(delta) {
-    // delta.start, delta.end, delta.lines, delta.action
-    console.log(delta);
-  });
-
-  this.editor.session.selection.on('changeSelection', function(delta) {
-    // delta.start, delta.end, delta.lines, delta.action
-    console.log(delta);
-  });
-
-  this.editor.session.selection.on('changeCursor', function(delta) {
-    // delta.start, delta.end, delta.lines, delta.action
-    console.log(delta);
-  });
 };
 
 EditorRecorder.prototype.start = function () {
